76582af793c8372a7068768a4ce024c81df3a52c
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-03-11 11:48:19 -0600

    add event listener for save btn

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@
 var today = dayjs().format('dddd MMMM D, YYYY');
 $('#currentDay').text(today);
 
+$('.saveBtn').on('click', function() {
+
+})
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
