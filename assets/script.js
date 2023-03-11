28d6520732266b2758db17a3d4f2c4faeaf928fa
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-03-11 11:14:18 -0600

    add today's date in header with Dayjs

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+
+var today = dayjs().format('dddd MMMM D, YYYY');
+$('#currentDay').text(today);
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
